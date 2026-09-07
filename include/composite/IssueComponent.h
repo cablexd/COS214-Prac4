80961322840ac28758dabe8a58ffd455ef6ea1e0
cablexd <cableplays1912@gmail.com>
2026-09-07 17:21:21 +0200

    add parent to IssueComponent

--- include/composite/IssueComponent.h
+++ include/composite/IssueComponent.h
@@ -7,28 +7,29 @@
 
 class Decorator;
 class Iterator;
+class IssueBucket;
 
 class IssueComponent
 {
-    friend class Decorator; // allow decorator to call getSnapshot() on any component type
+    friend class Decorator;   // allow decorator to call getSnapshot() on any component type
     friend class IssueBucket; // allow issue bucket to call getSnapshot() on any component type
     friend class Iterator;
 
 private:
-    virtual void getSnapshot(std::vector<IssueComponent*>& snapshot) = 0;
+    IssueBucket *parent;
+
+    virtual void getSnapshot(std::vector<IssueComponent *> &snapshot) = 0;
 
 public:
-    virtual Iterator *createIterator(std::string type);
-
-    virtual void print(int level) = 0;
-
-    virtual void printState() = 0;
-
-    virtual void execute() = 0;
-
-    virtual int getPriority();
-
+    IssueComponent();
     virtual ~IssueComponent();
+
+    IssueBucket *getParent();
+    virtual Iterator *createIterator(std::string type);
+    virtual void print(int level) = 0;
+    virtual void printState() = 0;
+    virtual void execute() = 0;
+    virtual int getPriority();
 };
 
 #endif
